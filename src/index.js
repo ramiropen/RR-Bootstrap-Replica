7321f565b714ca6db52cf68e3dc9c51ee1c218e9
unknown <ramiropen.work@gmail.com>
2022-04-23 18:41:40 -0700

    oh no

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Button from 'react-bootstrap/Button';
 
 ReactDOM.render(
   <React.StrictMode>
